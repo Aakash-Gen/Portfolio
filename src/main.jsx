87f0206bf7285c1158bfc949ae1bb477ea4bd5f2
Aakash Jain <aakashjain245@gmail.com>
2024-06-12 22:03:35 +0530

    added vercel web analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter,Route,Routes } from 'react-router-dom'
+import { inject } from '@vercel/analytics';
+ 
+inject();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
